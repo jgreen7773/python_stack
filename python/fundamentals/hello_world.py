558160f8b53d4f5429be06da3c53ddbe6c2b01b4
Jamie Green (Song Kran) <j.green7773@yahoo.com>
2019-09-02 13:07:07 -0700

    updated and finished the Hello World Python assignment.

--- python/fundamentals/hello_world.py
+++ python/fundamentals/hello_world.py
@@ -21,3 +21,26 @@ fave_food1 = "sushi"
 fave_food2 = "pizza"
 print("I love to eat {} and {}.".format(fave_food1, fave_food2))# with .format()
 print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
+
+
+
+
+
+
+
+myname = "Jamie Green"
+
+print("hello", myname)
+print("Hello " + myname)
+
+mynumber = 999
+
+print("Hello", mynumber)
+print("Hello " + mynumber)
+
+favfood1 = "avacado"
+favfood2 = "deli sandwiches"
+print("I love to eat {} and {}.".format(favfood1, favfood2))
+
+print(f"I love to eat {favfood1} and {favfood2}.")
+
