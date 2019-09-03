a27b20ddd19d178d7636a50efa4ca8da22e7aff2
Jamie Green (Song Kran) <j.green7773@yahoo.com>
2019-09-02 20:32:48 -0700

    Did my best, ran into PATH issues and googled for 5+ hours with few issues resolved in a satisfactory manner.

---
+++ python/fundamentals/for_loop_basic1.py
@@ -0,0 +1,30 @@
+for a in range(0, 151, 1):
+    print(a)
+
+
+for b in range(0, 1001, 5):
+    print(b)
+
+
+for c in range(0, 101, 1):
+    print(c)
+    if c[]/5 % 0:
+        print("Coding")
+    if c[]/10 % 0:
+        print("Coding Dojo")
+
+
+for d in range(1, 500001, 2):
+    e += [d]
+    return e
+
+
+for f in range(2018, == 0, -4):
+    print(f)
+
+
+lowNum = 22
+highNum = 4444
+mult = 11
+for g in range('lowNum', == 'highNum', 11):
+    print(g)
