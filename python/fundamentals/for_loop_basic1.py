b10c2d1037f9a9f3ce3bc32637eddaf71d0b4f6a
Jamie Green (Song Kran) <j.green7773@yahoo.com>
2019-09-03 09:41:38 -0700

    Modified my loops because I got vscode to actually 'test' my code, and am more proficient with the virtual environment in terminal.

--- python/fundamentals/for_loop_basic1.py
+++ python/fundamentals/for_loop_basic1.py
@@ -6,25 +6,28 @@ for b in range(0, 1001, 5):
     print(b)
 
 
-for c in range(0, 101, 1):
-    print(c)
-    if c[]/5 % 0:
+listconversion = []
+for n in range(0, len(listconversion), 1):
+    listconversion.append(int(n))
+    print(listconversion)
+    if listconversion[n] % 5 == 0:
         print("Coding")
-    if c[]/10 % 0:
+    if listconversion[n] % 10 == 0:
         print("Coding Dojo")
 
 
-for d in range(1, 500001, 2):
-    e += [d]
+def hugeNumber():
+    for d in range(1, 500001, 2):
+        e += [d]
     return e
 
 
-for f in range(2018, == 0, -4):
+for f in range(2018, 0, -4):
     print(f)
 
 
 lowNum = 22
 highNum = 4444
 mult = 11
-for g in range('lowNum', == 'highNum', 11):
-    print(g)
+for g in range('lowNum', 'highNum', 11):
+    print(int(g))
