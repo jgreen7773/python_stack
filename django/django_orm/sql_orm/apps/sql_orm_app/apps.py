7d2933e9ecc85ecd51323f376deb8deb5b447f19
Jamie Green (Song Kran) <j.green7773@yahoo.com>
2019-09-13 16:35:47 -0700

    Finished SQL Queries to ORM Assignment.

---
+++ django/django_orm/sql_orm/apps/sql_orm_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SqlOrmAppConfig(AppConfig):
+    name = 'sql_orm_app'
---
+++ django/django_orm/sql_orm/apps/sql_orm_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Hogwarts(models.Model):
+    name = models.CharField(max_length=45)
+    house = models.CharField(max_length=45)
+    pet = models.CharField(max_length=45)
+    year = models.IntegerField()
+
+
