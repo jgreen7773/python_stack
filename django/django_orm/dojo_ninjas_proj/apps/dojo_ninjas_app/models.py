734f11c76460bf3307a51104e236b0901ef61e97
Jamie Green (Song Kran) <j.green7773@yahoo.com>
2019-09-14 17:29:55 -0700

    Finished Dojo_&_Ninjas (Shell) Assignment.

---
+++ django/django_orm/dojo_ninjas_proj/apps/dojo_ninjas_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    desc = models.TextField()
+
+
+class ninjas(models.Model):
+    dojo_id = models.ForeignKey(dojos, related_name="attending_dojo")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
