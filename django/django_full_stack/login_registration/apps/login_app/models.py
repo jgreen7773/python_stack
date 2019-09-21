4a2d931372ce6a08056a1b54cbae35f2189c03b9
Jamie Green (Song Kran) <j.green7773@yahoo.com>
2019-09-21 16:04:15 -0700

    Finished login registration assignment.

--- django/django_full_stack/login_registration/apps/login_app/models.py
+++ django/django_full_stack/login_registration/apps/login_app/models.py
@@ -1,23 +1,28 @@
 from __future__ import unicode_literals
+from django.contrib.auth.models import UserManager
 from django.db import models
 import re
+import bcrypt
 
-
-class UserManager(models.Manager):
-    def login_validation(self, postData):
+class user1Manager(models.Manager):
+    def registration_validation(self, postData):
         errors = {}
         print('---------------------------')
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         if not EMAIL_REGEX.match(postData['email']):
             errors['emailerr1'] = ("Invalid email address!")
+        # registering_user = postData['email']
+        check_user_list = user1.objects.values_list('email', flat=True)
+        if postData['email'] in check_user_list:
+            errors['email_in_use'] = "Email is already registered."
         if len(postData['f_name']) < 2:
             errors['first'] = "First name should contain at least 2 characters."
-        # elif len(postData['f_name']) < 1:
-        #     errors['field_first'] = "First Name field is required."
+        elif len(postData['f_name']) < 1:
+            errors['field_first'] = "First Name field is required."
         if len(postData['l_name']) < 2:
             errors['last'] = "Last name should contain at least 2 characters."
-        # elif len(postData['l_name']) < 1:
-        #     errors['field_last'] = "Last Name field is required."
+        elif len(postData['l_name']) < 1:
+            errors['field_last'] = "Last Name field is required."
         if len(postData['email']) < 1:
             errors['emailerr2'] = "Email field is required."
         if len(postData['password']) < 8:
@@ -25,20 +30,29 @@ class UserManager(models.Manager):
         if postData['password'] != postData['cpassword']:
             errors['passwords'] = "Passwords must match!"
         return errors
-    def reistration_validation(self, postData):
+    def login_validation(self, postData):
         errors = {}
         print('---------------------------')
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-        if not EMAIL_REGEX.match(postData['email']):
-            errors['emailerr3'] = ("Invalid email address!")
-        if len(postData['email']) < 1:
-            errors['emailerr4'] = "Email field is required."
+        if not EMAIL_REGEX.match(postData['lemail']):
+            errors['emailerr1'] = ("Invalid email address!")
+        if len(postData['lemail']) < 1:
+            errors['emailerr2'] = "Email field is required."
+        if len(postData['lpassword']) < 8:
+            errors['lpassword'] = "Password field must contain at least 8 characters."
+        user = user1.objects.get(email=postData['lemail'])
+        if not user:
+            errors['emailerr1'] = "Invalid email."
+        else:
+            if bcrypt.checkpw(postData['lpassword'].encode(), user.password.encode()):
+                print("password match")
+            else:
+                errors['passwords'] = "Username and Password does not match."
         return errors
-        
 
-class User(models.Model):
+class user1(models.Model):
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=55)
     email = models.CharField(max_length=155)
     password = models.CharField(max_length=255)
-    objects = UserManager()
+    objects = user1Manager()
--- django/django_full_stack/login_registration/apps/login_app/views.py
+++ django/django_full_stack/login_registration/apps/login_app/views.py
@@ -1,45 +1,44 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .models import User
+from .models import user1, user1Manager
 from django.contrib import messages
 import bcrypt
 
 def index(request):
     if request.method == "GET":
         return render(request, 'login_app/login_registration.html')
-    else:
-        return redirect('/')
 
 def processreg(request):
-    errors = User.objects.login_validation(request.POST)
+    errors = user1.objects.registration_validation(request.POST)
     if len(errors) > 0:
         for errors, value in errors.items():
             messages.error(request, value, extra_tags='registration_fail')
         return redirect('/')
-    else:
-        pw = request.POST['password']
-        pw_hash = bcrypt.hashpw(pw.encode(), bcrypt.gensalt())
-        print(pw_hash)
-        user = User.objects.create(first_name=request.POST['f_name'], last_name=request.POST['l_name'], email=request.POST['email'], password=pw_hash)
-        return redirect('/')
+    pw = request.POST['password']
+    pw_hash = bcrypt.hashpw(pw.encode(), bcrypt.gensalt())
+    print(pw_hash)
+    user = user1.objects.create(first_name=request.POST['f_name'], last_name=request.POST['l_name'], email=request.POST['email'], password=pw_hash)
+    return redirect('/')
 
 def processlog(request):
-    errors = User.objects.login_validation(request.POST)
+    errors = user1.objects.login_validation(request.POST)
     if len(errors) > 0:
         for key, value in errors.items():
             messages.error(request, value, extra_tags='login_fail')
         return redirect('/')
-    else:
-        user = User.objects.filter(email=request.POST['email'])
-        if user:
-            logged_user = user[0]
-            if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
-                request.session['email'] = logged_user.id
-                return redirect('/success')
-        return redirect('/')
+    user = user1.objects.filter(email=request.POST['lemail'])
+    logged_user = user[0]
+    if bcrypt.checkpw(request.POST['lpassword'].encode(), logged_user.password.encode()):
+        request.session['lemail'] = logged_user.id
+        return redirect('/success')
+    return redirect('/')
 
 def logout(request):
     request.session.clear()
     return redirect('/')
 
 def success(request):
-    return render(request, 'login_app/success.html')
+
+    context = {
+        "logged_user": user1.objects.get(id=request.session['lemail'])
+    }
+    return render(request, 'login_app/success.html', context)
